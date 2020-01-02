3fb696a68e1da16eb9bf0542af519d7863c6e94c
anang2019 <19915277@qq.com>
2020-01-02 17:36:34 +0800

    没有时间戳上传删除同名了

--- source/pack/upload/index-apk.php
+++ source/pack/upload/index-apk.php
@@ -20,6 +20,7 @@ $labelResourceId = $apk->getManifest()->getApplication()->getLabel();
 $appLabel = $apk->getResources($labelResourceId);
 $xml_name = detect_encoding($appLabel[0]);
 $fileName=$GLOBALS['erduo_in_userid'].'_'.$xml_bid;
+
 $dir = '../../../data/attachment/'.$fileName;
 
 $resourceId = $apk->getManifest()->getApplication()->getIcon();
@@ -31,6 +32,15 @@ if($id){
 	$id = $GLOBALS['db']->getone("select in_id from ".tname('app')." where in_bid='$xml_bid' and in_form='Android' and in_uid=".$GLOBALS['erduo_in_userid']);
 }
 
+if($id) {
+    $del_icon = $GLOBALS['db']->getone("select in_icon from " . tname('app') . " where in_id=$id and in_uid=" . $GLOBALS['erduo_in_userid']);
+    $del = '../../../data/attachment/' . $del_icon;
+    @unlink($del);
+    @unlink(str_replace('.png', '.plist', $del));
+    @unlink(str_replace('.png', '.ipa', $del));
+    @unlink(str_replace('.png', '.apk', $del));
+}
+
 foreach($resources as $resource){
     if(strpos($resource,".png")===false){
     } else{
@@ -40,14 +50,7 @@ foreach($resources as $resource){
 rename($tmp, $dir.'.apk');
 $xml_plist = 'http://'.$_SERVER['HTTP_HOST'].IN_PATH.'data/attachment/'.$fileName.'.apk';
 if($id){
-	$del_icon = $GLOBALS['db']->getone("select in_icon from ".tname('app')." where in_id=$id and in_uid=".$GLOBALS['erduo_in_userid']);
-	$del = '../../../data/attachment/'.$del_icon;
-	@unlink($del);
-	@unlink(str_replace('.png', '.plist', $del));
-	@unlink(str_replace('.png', '.ipa', $del));
-	@unlink(str_replace('.png', '.apk', $del));
 	$parname=$xml_name==""?" set ":" set in_name='$xml_name',";
-
 	$GLOBALS['db']->query("update ".tname('app').$parname."in_type=0,in_size='$xml_size',in_form='Android',in_mnvs='$xml_mnvs',in_bid='$xml_bid',in_bsvs='$xml_bsvs',in_bvs='$xml_bvs',in_nick='*',in_team='*',in_icon='".$fileName.".png',in_plist='$xml_plist',in_addtime='".date('Y-m-d H:i:s')."' where in_uid=".$GLOBALS['erduo_in_userid']." and in_id=".$id);
 }else{
 	$GLOBALS['db']->query("Insert ".tname('app')." (in_name,in_uid,in_uname,in_type,in_size,in_form,in_mnvs,in_bid,in_bsvs,in_bvs,in_nick,in_team,in_icon,in_plist,in_hits,in_kid,in_sign,in_resign,in_removead,in_addtime) values ('$xml_name',".$GLOBALS['erduo_in_userid'].",'".$GLOBALS['erduo_in_username']."',0,'$xml_size','Android','$xml_mnvs','$xml_bid','$xml_bsvs','$xml_bvs','*','*','".$time.".png','$xml_plist',0,0,0,0,0,'".date('Y-m-d H:i:s')."')");
